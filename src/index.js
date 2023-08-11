718d38fd550b3f19584ed460f551f94d50fd9e0f
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-08-11 10:32:01 +0300

    qwe

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import temlate from './templates/template.hbs'
+const showBlock = document.querySelector('#infotest')
+console.log("😎 ~ temlate:", temlate({ title: "My New Post", body: "This is my first post!" }));
+showBlock.insertAdjacentHTML('beforeend', temlate({ title: "My New Post", body: "This is my first post!" }))
