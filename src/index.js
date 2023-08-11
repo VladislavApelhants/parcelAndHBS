a39f7224a24eb59f179fb7b1179aea85fa95f854
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-08-11 11:46:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import Handlebars from "handlebars";
-console.log("😎 ~ Handlebars:", Handlebars);
-import temlate from './templates/template.hbs'
 const showBlock = document.querySelector('#infotest')
-console.log("😎 ~ temlate:", temlate({ title: "My New Post", body: "This is my first post!" }));
-showBlock.insertAdjacentHTML('beforeend', temlate({ title: "My New Post", body: "This is my first post!" }))
+
+import template from './templates/template.hbs'
+console.log("😎 ~ template:", template);
+
+
+showBlock.insertAdjacentHTML('beforeend', template({ title: "My New Post", body: "This is my first post!" }))
