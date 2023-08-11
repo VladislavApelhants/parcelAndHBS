68a8caeeb4ec9434350c7c1e69efea12438acbd9
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-08-11 11:22:51 +0300

    wef

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Handlebars from "handlebars";
+console.log("😎 ~ Handlebars:", Handlebars);
 import temlate from './templates/template.hbs'
 const showBlock = document.querySelector('#infotest')
 console.log("😎 ~ temlate:", temlate({ title: "My New Post", body: "This is my first post!" }));
